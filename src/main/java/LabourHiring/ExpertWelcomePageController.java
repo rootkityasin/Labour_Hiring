826cd06fe9ed407aa9d260725d7840d6ee1c055e
irinnisha <71202635+irinnisha@users.noreply.github.com>
2023-07-30 22:31:22 +0600

    ..

--- src/main/java/LabourHiring/ExpertWelcomePageController.java
+++ src/main/java/LabourHiring/ExpertWelcomePageController.java
@@ -1,8 +1,37 @@
 package LabourHiring;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
 
 public class ExpertWelcomePageController {
-    public void onActionInProfileButton(ActionEvent actionEvent) {
+
+    @FXML
+    private Button expertProfileButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private SplitPane panel1;
+
+    @FXML
+    private ImageView profileImage;
+
+    @FXML
+    private Circle profilePicCircle;
+
+    @FXML
+    void onActionInProfileButton(ActionEvent event) {
+
     }
+
+    @FXML
+    void onActionLogout(ActionEvent event) {
+
+    }
+
 }
---
+++ src/main/java/LabourHiring/userProfilePage.java
@@ -0,0 +1,4 @@
+package LabourHiring;
+
+public class userProfilePage {
+}
