646deadd1dde235853080c106ccf85fbd5c3f77f
Saide Bin Sabid <112235402+saidesabid@users.noreply.github.com>
2023-08-25 23:58:03 +0600

    aLL

--- src/main/java/LabourHiring/searchResult.java
+++ src/main/java/LabourHiring/searchResult.java
@@ -2,14 +2,10 @@ package LabourHiring;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
+
 import javafx.stage.Stage;
 
 import java.util.Objects;
